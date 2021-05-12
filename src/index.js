f2aae607d4f9dfb3bbad26e689112d979e34b4e9
Alberto <ajsaenz1091@gmail.com>
2021-05-11 21:28:39 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -19,45 +19,117 @@ document.addEventListener("DOMContentLoaded", () => {
 const toysUrl = 'http://localhost:3000/toys'
 
 
-//* This function handles the form submission and passed as the second parameter of an event handler
+// //* This function handles the form submission and passed as the second argument of an event handler
 function submitDataHandler(){
   let formName = form.elements.namedItem("name").value;
   let formSrc = form.elements.namedItem("image").value;
-  let formLikes = form.elements.namedItem("submit").value
-  formData = {
-    name: formName,
-    image: formSrc,
-    likes: formLikes 
+  let formData = {
+    "name": formName,
+    "image": formSrc,
+    "likes": 0
+  };
+  const configObject = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
   }
+  console.log(formName, formSrc)
+  formData.name = formName;
+  postToys(configObject);
 }
+// // //* DATA RETRIEVED FROM THE FORM
 
-//* DATA RETRIEVED FROM THE FORM
+// let formData = {
+//   "name": formName,
+//   "image": formSrc,
+//   "likes": 10000
+// };
+
+// //* CONFIGURATION OBJECT FOR THE POST REQUEST
+// const configObject = {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// }
+
+// const consfigObj = {
+//   "name": "Jessie",
+//   "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+//   "likes": 0
+// }
+
 let form = document.querySelector(".add-toy-form")
 form.addEventListener('submit', submitDataHandler);
 
-let formData = {
-  name: "",
-  image: "",
-  likes: 0
-};
-
-//* CONFIGURATION OBJECT FOR THE POST REQUEST
-const configObject = {
-  method: 'POST',
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
+function postToys(configObject){
+  fetch(toysUrl, configObject)
+  .then(res => res.json())
+  .then(newToy => addToCollection(newToy))
 }
 
 
+function addToCollection(toy){
+    let name = toy.name
+    let srcUrl = toy.image
+    let likes = toy.likes
+    let id = toy.id
+    let toyCollection = document.querySelector('#toy-collection')
+    let cardContainer = document.createElement('div')
+    cardContainer.setAttribute('class', 'card')
+    let hTag = document.createElement('h2');
+    hTag.innerHTML = name;
+    let img = document.createElement('img');
+    img.setAttribute('class','toy-avatar')
+    img.src = srcUrl;
+    let p = document.createElement('p');
+    p.innerHTML = likes;
+    let likeButton = document.createElement('button');
+    likeButton.setAttribute('class', 'like-btn')
+    likeButton.setAttribute('id', id)
+
+    cardContainer.append(hTag,img,p,likeButton)
+    toyCollection.appendChild(cardContainer);
+}
 
 
 function fetchToys(){
-  return fetch(toysUrl, configObject)
+   fetch(toysUrl)
   .then(res => res.json())
   .then(toysData => createCards(toysData))
+}
+
+function handleLikes(event){
+
+  let id = event.target.id;
+
+  let likesField = event.target.parentElement.querySelector('p');//*grabs the p tag of the clicked card
+
+  let cardLikes = parseInt(likesField.innerHTML); //*grabs the inner html of the p tag of the clicked card
+  console.log(cardLikes)
+
+
+  //* BUILD DATA OBJECT TO PASS ON TO CONFIG OBJECt
+  likesData = {
+      "likes": cardLikes + 1  //* we are changing the "likes" value to one more evry click
+  }
+  //* BUILD CONFIGURATION OBJECT FOR PATCH REQUEST
+  const patchObj = { // note we are going to 
+    method: "PATCH",
+    headers: {
+        "Content-Type" : "application/json"
+      },
+    body: JSON.stringify(likesData)
+  }
+  fetch(`http://localhost:3000/toys/${id}`,patchObj)
+  .then(res => res.json())
+  .then(toyObj => likesField.innerHTML = toyObj.likes)//* updates likes in DOM before patching the change to the database
+  
 }
 
 
@@ -81,6 +153,7 @@ function createCards(toysObjsArr){
     let likeButton = document.createElement('button');
     likeButton.setAttribute('class', 'like-btn')
     likeButton.setAttribute('id', id)
+    likeButton.addEventListener('click', handleLikes)
 
     cardContainer.append(hTag,img,p,likeButton)
     toyCollection.appendChild(cardContainer);
